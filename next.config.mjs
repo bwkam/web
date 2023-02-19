c97a07c256c9edef78f981ac0b85e5e137d69039
bwkm <userdev987@gmail.com>
2023-02-19 21:46:04 +0200

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import remarkFrontmatter from 'remark-frontmatter';
 import rehypeHighlight from 'rehype-highlight';
 
 export default {
+   
   webpack: (config, options) => {
     config.resolve.fallback = { fs: false };
     config.module.rules.push({
@@ -23,4 +24,17 @@ export default {
   },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  
 };
+
+ const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+    let assetPrefix = ''
+    let basePath = '/'
+
+    if (isGithubActions) {
+      const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+      assetPrefix = `/${repo}/`
+      basePath = `/${repo}`
+}
